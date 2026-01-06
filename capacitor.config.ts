d0e4fadf6e80cba56ff6f6d3bf42d08ea27c7a38
Douglas216 <douglasj216@outlook.com>
2026-01-06 20:18:27 +0800

    edits after hanging out with Nancy

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.douglas.lifemaxxing',
+  appName: 'LifeMaxxing',
+  webDir: 'build'
+};
+
+export default config;
